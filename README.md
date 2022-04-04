d5fd2b4748e03694a747e2acdde848e0ad7498e9
Tiffany <tiff.sobczak@gmail.com>
2022-04-03 22:44:55 -0500

    added screenshot

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Tiffany Sobczak - Portfolio
 
 ## Screenshot
-![Portfolio Screenshot]()
+![Portfolio Screenshot](public/assets/images/portfSS.png)
 
 
 ## Description
@@ -10,5 +10,5 @@ Tiffany Sobczak's portfolio website featuring her most recent projects.
 
 ## Access
 * GitHub Repository: https://github.com/tiffsobczak/tsobczak-portfolio
-* Deployed Application: 
+* Deployed Application: https://tiffsobczak.github.io/tsobczak-portfolio/
 
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,10 +9,10 @@ function About() {
       <div className="my-2">
       <Avatar alt="Tiffany" src={process.env.PUBLIC_URL+"/assets/images/christmas-photo.jpeg"} sx={{ width: 100, height: 100, marginBottom: 2 }} />
         <p>
-          Professional: Full Stack Web Developer with a certificate from Washington University in St. Louis. 
+          Professional: Full Stack Web Developer with a certificate from Washington University in St. Louis. Former public school teacher for five years with a Bachelor's of Science in Education with a minor in Mathematics and Social Studies from Southeast Missouri State University.
         </p>
         <p>Personal: After moving from Wisconsin to Missouri, I left teaching in public schools after five years.
-          Health and fitness are a big passion of mine - as much as finding the best local ice cream shops. My life long dream is to own a dessert bakery - but I have no idea how to bake. </p>
+          Health and fitness are a big passion of mine - as much as finding the best local ice cream shops. My life long dream is to own a dessert bakery - but I have no idea how to bake. I love dogs, high ropes courses, and watching The Office on repeat.</p>
       </div>
     </section>
   );
