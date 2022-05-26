ff27cfe8439b813021ea603a82750352c4c93622
Tiffany <tiff.sobczak@gmail.com>
2022-05-26 15:00:08 -0500

    updated bio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,10 +9,10 @@ function About() {
       <div className="my-2">
       <Avatar alt="Tiffany" src={process.env.PUBLIC_URL+"/assets/images/christmas-photo.jpeg"} sx={{ width: 100, height: 100, marginBottom: 2 }} />
         <p>
-          Professional: Full Stack Web Developer with a certificate from Washington University in St. Louis. Former public school teacher for five years with a Bachelor's of Science in Education with a minor in Mathematics and Social Studies from Southeast Missouri State University.
+          Professional: In April of 2022, I completed a Full-Stack Web Development Bootcamp through Washington University in St. Louis (WashU). The program focused heavily on JavaScript. We created a plethora of projects using Node, Express, SQL, React, HTML, and CSS. There were weekly assignments and three group projects throughout the 24 week course.
         </p>
         <p>Personal: After moving from Wisconsin to Missouri, I left teaching in public schools after five years.
-          Health and fitness are a big passion of mine - as much as finding the best local ice cream shops. My life long dream is to own a dessert bakery - but I have no idea how to bake. I love dogs, high ropes courses, and watching The Office on repeat.</p>
+          Health and fitness is a big passion of mine - as much as finding the best local ice cream shop. My life long dream is to own a dessert bakery - but I have no idea how to bake. I love dogs, high ropes courses, and watching The Office. I love a good challenge whether it be teaching myself to sew or doing 100 burpees.</p>
       </div>
     </section>
   );
